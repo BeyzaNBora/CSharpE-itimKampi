dd1953958ab3c84460007294aee8e9f437558c92
BeyzaNBora <134893400+BeyzaNBora@users.noreply.github.com>
2024-10-02 21:45:08 +0300

    Add project files.

---
+++ 01_MainSubjects/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_MainSubjects
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			#region YazdırmaKomutları
+			//Console.Write("Merhaba Dünya");
+			//Console.WriteLine("Selam");
+			//Console.Write("hello");
+
+			//Console.WriteLine("***** Yemek Kategorileri *****");
+			//Console.WriteLine();
+			//Console.WriteLine("1-Çorbalar");
+			//Console.WriteLine("2-Ana Yemekler");
+			//Console.WriteLine("3-Soğuk Başlangıçlar");
+			//Console.WriteLine("4-Salatalar");
+			//Console.WriteLine("5-Tatlılar");
+			//Console.WriteLine("6-İçecekler");
+			//Console.WriteLine();
+			//Console.WriteLine("***** Yemek Kategorileri *****");
+
+			#endregion
+
+			#region String Değişkenler
+
+			//string
+
+			//string name = "beyza";
+			//name = "beto";
+			//console.writeline(name);
+
+			//string customerName;
+			//string customerSurname;
+			//string customerPhone;
+			//string customerEmail, district, city;
+
+			//customerName = "Beyza";
+			//customerSurname = "Bora";
+			//customerPhone = "05438055894";
+			//customerEmail = "beyzaabora@hotmail.com";
+			//district = "Akyurt";
+			//city = "Ankara";
+
+			//Console.WriteLine("***** Rezervasyon Kartı *****");
+			//Console.WriteLine();
+			//Console.WriteLine("-----------------------------");
+			//Console.WriteLine("Müşteri: " + customerName + "" + customerSurname);
+			//Console.WriteLine("İletişim: " + customerPhone);
+			//Console.WriteLine("Email Adresi"+customerEmail);
+			//Console.WriteLine("Adres: " + district + "/" + city);
+			//Console.WriteLine("-----------------------------");
+
+			#endregion
+
+			#region Int Değişkenler
+
+
+
+			#endregion
+
+			Console.Read();
+		}
+	}
+}
+
+
